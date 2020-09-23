e27a65ad55e1d078c62b4deca9e42538d0212d31
Ferron Stasse <99052055@mydavinci.nl>
2020-09-23 12:30:00 +0200

    veder gewerkt met game

--- game.js
+++ game.js
@@ -2,17 +2,17 @@ var start = prompt("Wil je deze game spelen? Beantwoord met Ja of Nee");
 var LowerStart = start.toLowerCase();
 
 if(LowerStart == "ja"){
-	var level1 = prompt("Je ben aangekomen in altanties, kiest uit deur 1 2 of 3");
+	var level1 = prompt("Je ben aangekomen in altanties, kiest uit verschillende routes 1 2 of 3");
 	var Lowerlevel1 = parseInt(level1)
 
 	if(Lowerlevel1 == 1 || Lowerlevel1 == 3){
-		alert("Je heb de verkeerd deur gekozen");
+		alert("Je heb de verkeerd route gekozen");
 		    document.write("Je ben bij level 1 gekomen!" + "<br>");
 		    document.write("Reload jou pagina om opnieuwe te beginnnen!");
 		}
 
 		else if(Lowerlevel1 == 2){
-			var level2 = prompt("Je heb de goede deur gekozen!, We komen een aardige persoon tegen die graag een apple wil. ga opzoek naar een apple, Voer links of right in.");
+			var level2 = prompt("Je heb de goede route gekozen!, We komen een aardige persoon tegen die graag een apple wil. ga opzoek naar een apple, Voer links of right in.");
 			var lowerlevel2 = level2.toLowerCase();
         }
 
@@ -52,11 +52,58 @@ if(LowerStart == "ja"){
         }
 
         if(lowerlevel6 == "go" || lowerlevel5 == "go"){
-        	var level7 = prompt("text");
-        }
+        	var level7 = prompt("Je ben aangekomen bij een nieuwe stad, Ze vragen om jou hulp, Voer: Mining of Jagen in.");
+        	var lowerlevel7 = level7.toLowerCase();
+        	if(lowerlevel7 == "mining"){
+        		var level8 = prompt("je ben aangekomen bij de mine je heb al 100 Euro verdien.Voer: Doorgaan of stoppen in.");
+        		var lowerlevel8 = level8.toLowerCase();
+        		if(lowerlevel8 == "doorgaan");{
+        		var level9 = prompt("je ben doorgaan zoeken en je heb een diamond gevonden. Voer: kingdom opzoeken of person");
+        		var lowerlevel9 = level9.toLowerCase();
 
+        		if(lowerlevel9 == "kingdom opzoeken"){
+        			var level12 = prompt("je heb een plek gevonden om een kingdom op te zetten. wat wil je als eerst neerzetten. Voer: Farms of muren?");
+        			var lowerlevel12 = level12.toLowerCase();
+        		}
+        		else if(lowerlevel9 == "person"){
+                    alert("Je heb genoeg geld verdien om nooit meer te werken en je heb gewonnen.");
+                        document.write("Gefilteerd je heb gewonnen!" + "<br>");
+                        document.write("Er zijn nog andere manieren om verder te gaan en om het spel op andere manieren uit te spelen");
+                    }
+                }
+
+                    else if(lowerlevel8 == "stoppen"){
+        		        var level10 = prompt("Je ben gestopd en terug gegaan naar de stad en je heb 100 euro. Je gaat naar een smite. Voer: Boog of zwaard in.");
+        	          	var lowerlevel10 = level10.toLowerCase();
+        	        	if(lowerlevel10 == "zwaard"){
+        		           	alert("Je heb leren om met een zwaard om te gaan en nu zit je in het leger en je commander van de leger");
+        			        document.write("Gefilteerd je heb gewonnen en je ben nu een gezonde commander van het leger");
+        	        	}
+
+        		else if(lowerlevel10 == "boog"){
+        			alert("Je heb leren boog schieten en je kan nu jagen of dieren en je heb daar je beroep van gemaakt.");
+        			document("Gefilteerd je heb een beroep van jager gemaakt.")
+        		}
+        	}
+                    
+
+                
+            
+
+        if(lowerlevel12 == "farms"){
+        	alert("Je heb de farms opgebouwd en je volk is gezond en alles gaat goed als gepland");
+        	document.write("Farms zijn gemaakt en het volk is gezond en alled gaat goed als gepland" + "<br>");
+        	document.write("Je heb gewonnen om een gezond volk op te bouwen.");
+        }
+        else if(lowerlevel12 == "muren"){
+        	alert("Je heb de muren gebouwd maar je was te laat om genoeg eten te maken voor je volk");
+        	document.write("Je volk is verhongerd en je andere volk die het hebben overleefd hebben jou verlaten en naar andere kingdom gegaan" + "<br>");
+        	document.write("Probeer het nog eens :)");
+        }
+    }
+        
 
 else if(LowerStart == "nee"){
 	alert("Je wilde het spel niet spelen.");
 	    document.write("Reload jou pagina om te beginnen!.");
-}
+    }
